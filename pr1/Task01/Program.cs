39e4a12a56f8e4f8c0ce80e5fa579cf60a59382d
Serzh <Lipiev@mail.ru>
2022-08-24 09:37:19 +0300

    C# Практическое задание 2

---
+++ pr1/Task01/Program.cs
@@ -0,0 +1,42 @@
+﻿//----Задача 2----
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b) {
+    Console.WriteLine("Первое число больше");
+} else {
+    Console.WriteLine("Второе число больше");
+}
+
+//----Задача 4----
+
+Console.WriteLine("Введите первое число");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int z = Convert.ToInt32(Console.ReadLine());
+
+int max = x;
+if (y > max) max = y;
+if (z > max) max = z;
+
+Console.WriteLine($"Большее число {max}");
+
+//----Задача 6----
+
+Console.WriteLine("Введите число");
+int s = Convert.ToInt32(Console.ReadLine());
+int t = s % 2;
+if (t == 0) Console.WriteLine("Делится");
+else Console.WriteLine("Не делится");
+
+//----Задача 8----
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++) {
+    if (i % 2 == 0) Console.Write($"{i} ");
+}
---
+++ pr2/task/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 10
+Console.WriteLine("Введите трехзначное число");
+int digits = System.Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Второе число = {digits / 10 % 10}");
+
+// Задача 13
+Console.WriteLine("Введите число");
+string str = Console.ReadLine()!;
+int len = str.Length;
+if (len <= 2)
+    Console.WriteLine("Третей цифры нет");
+else 
+    Console.WriteLine($"Третяя цифра {str[2]}");
+
+// Задача 15
+Console.WriteLine("Введите цифру, обозначающую день недели 1-7");
+int day = System.Convert.ToInt32(Console.ReadLine());
+if (day > 7 || day < 1) {
+    Console.WriteLine("Такого дня недели не существует");
+} else if (day > 5) {
+    Console.WriteLine("Да");
+} else {
+    Console.WriteLine("Нет");
+}
