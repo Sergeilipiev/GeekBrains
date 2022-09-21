0d05f534ab40e0a0305eeebd47a73241c3322fe7
Serzh <Lipiev@mail.ru>
2022-09-21 11:00:42 +0300

    Решение задачи

---
+++ Final work C#/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] words = new string[4] {"The", "final", "work", "C#"};
+string tempStr = "";
+
+void PrintArray (string[] arr) {
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+for (int i = 0; i < words.Length; i++)
+    if (words[i].Length <= 3) 
+        tempStr+=words[i]+",";
+
+string[] wordsRes = new string[tempStr.Split(",").SkipLast(1).ToArray().Length];
+wordsRes = tempStr.Split(",").SkipLast(1).ToArray();
+
+PrintArray(words);
+PrintArray(wordsRes);
